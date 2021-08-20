cb86a71e9497535b9658a95a7e2aad59a221ad33
mykie88 <mykhelmyers@gmail.com>
2021-08-20 09:26:10 +0100

    first commit

---
+++ resume/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import redirect, render
+from .forms import ContactForm
+from django.contrib import messages
+
+# Create your views here.
+
+def home(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            name = request.POST.get('name')
+            form.save()
+            messages.info(request, f"Thanks {name} for your message, I will get back to you shortly")
+            return redirect('home')
+    else:
+        form = ContactForm()
+    return render(request, 'home.html', {'form': form})
